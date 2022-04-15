6b728c129a3cd6ecdfedf6df12e7e99f32db1592
Magomed Kasumov <kasumov82@gmail.com>
2022-04-15 12:37:27 +0300

    Some changes in test_parser.py and a new file website.txt

--- lesson_3/testing/test_parser.py
+++ lesson_3/testing/test_parser.py
@@ -1,6 +1,15 @@
+from unittest.mock import patch, Mock
 from pytest import fixture
 from bs4 import BeautifulSoup
 from lesson_3.parser import YcombinatorParser
+
+
+class Response:
+    @property
+    def text(self):
+        with open('website.txt', 'r') as file:
+            file_text = file.read()
+        return file_text
 
 
 @fixture
@@ -9,18 +18,26 @@ def parser() -> YcombinatorParser:
 
 
 class TestParser:
-    def test_get_soup(self, parser):
+    @patch.object(YcombinatorParser, 'get_response')
+    def test_get_soup(self, mock_get_response, parser):
+        mock_get_response.return_value = Response()
         soup = parser.get_soup()
         assert isinstance(soup, BeautifulSoup) is True
 
-    def test_get_links(self, parser):
+    @patch.object(YcombinatorParser, 'get_response')
+    def test_get_links(self, mock_get_response, parser):
+        mock_get_response.return_value = Response()
         links = parser.get_links()
         assert links is not None
 
-    def test_one_link(self, parser):
+    @patch.object(YcombinatorParser, 'get_response')
+    def test_one_link(self, mock_get_response, parser):
+        mock_get_response.return_value = Response()
         link = parser.get_links()[0]
         assert link.startswith('https') is True
 
-    def test_get_inner_links(self, parser):
+    @patch.object(YcombinatorParser, 'get_response')
+    def test_get_inner_links(self, mock_get_response, parser):
+        mock_get_response.return_value = Response()
         inner_links = parser.get_inner_links(parser.get_links())
         assert inner_links is not None
