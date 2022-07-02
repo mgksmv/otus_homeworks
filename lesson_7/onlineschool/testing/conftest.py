bad0f223487b0b27f97f4fad36eb8e8eb69516e2
Magomed Kasumov <kasumov82@gmail.com>
2022-07-02 14:56:43 +0300

    Add some tests

--- lesson_7/onlineschool/testing/conftest.py
+++ lesson_7/onlineschool/testing/conftest.py
@@ -12,3 +12,25 @@ def user(db):
 @pytest.fixture()
 def login_user(db, user, client):
     return client.login(email='test@test.com', password='test12345')
+
+
+@pytest.fixture()
+def user_teacher(db):
+    return User.objects.create(
+        email='teacher@test.com',
+        first_name='Test',
+        last_name='Teacher',
+        password='teacher12345',
+        user_type='1',
+    )
+
+
+@pytest.fixture()
+def user_student(db):
+    return User.objects.create(
+        email='student@test.com',
+        first_name='Test',
+        last_name='Student',
+        password='student12345',
+        user_type='2',
+    )
--- lesson_7/onlineschool/testing/general_tests.py
+++ lesson_7/onlineschool/testing/general_tests.py
@@ -17,6 +17,29 @@ class Tests:
         all_users = User.objects.all()
         assert all_users.count() == 1
 
+    def test_signup_user(self, client):
+        response = client.post('/accounts/signup/', {
+            'email': 'test2@test.com',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'birthday': '1999-09-09',
+            'phone': 88005553535,
+            'user_type': '1',
+            'password1': 'test111222333',
+            'password2': 'test111222333',
+        })
+        assert response.status_code == 302
+        assert User.objects.all().count() == 1
+        assert User.objects.get(email='test2@test.com').first_name == 'Test'
+        assert User.objects.get(email='test2@test.com').last_name == 'User'
+
+    def test_login_user(self, client, user):
+        response = client.post('/accounts/login/', {
+            'username': 'test@test.com',
+            'password': 'test12345',
+        })
+        assert response.status_code == 302
+
     def test_home_page(self, client):
         response = client.get('/')
         assert response.status_code == 200
@@ -26,6 +49,11 @@ class Tests:
         response = client.get('/contact/')
         assert response.status_code == 200
         assert 'Связь с нами'.encode('utf-8') in response.content
+
+    def test_api_token_page(self, client, login_user):
+        response = client.get('/api-token/')
+        assert response.status_code == 200
+        assert b'/api/v1/' in response.content
 
     def test_search_nonexistent_course(self, client):
         response = client.get('/courses/search/?keyword=asdasdasd')
@@ -40,4 +68,34 @@ class Tests:
 
     def test_forbidden_page(self, client, login_user):
         response = client.get('/courses/my-courses/')
+        assert response.status_code == 403
+
+    def test_forbidden_my_courses_page_for_teacher(self, client, user_teacher):
+        client.force_login(user_teacher)
+        response = client.get('/courses/my-courses/')
+        assert response.status_code == 403
+
+    def test_forbidden_create_course_page_for_student(self, client, user_student):
+        client.force_login(user_student)
+        response = client.get('/courses/create-course/')
+        assert response.status_code == 403
+
+    def test_forbidden_create_category_page_for_student(self, client, user_student):
+        client.force_login(user_student)
+        response = client.get('/courses/create-category/')
+        assert response.status_code == 403
+
+    def test_forbidden_create_group_page_for_student(self, client, user_student):
+        client.force_login(user_student)
+        response = client.get('/courses/create-group/')
+        assert response.status_code == 403
+
+    def test_forbidden_schedule_page_for_student(self, client, user_student):
+        client.force_login(user_student)
+        response = client.get('/courses/schedule/')
+        assert response.status_code == 403
+
+    def test_forbidden_registration_requests_page_for_student(self, client, user_student):
+        client.force_login(user_student)
+        response = client.get('/courses/registration-requests/')
         assert response.status_code == 403
