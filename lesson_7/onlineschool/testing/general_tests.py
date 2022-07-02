a4b9b4526854d4980f35c88430dbdca402d7759c
Magomed Kasumov <kasumov82@gmail.com>
2022-07-02 12:26:19 +0300

    Fix templates dirs

--- lesson_7/onlineschool/testing/general_tests.py
+++ lesson_7/onlineschool/testing/general_tests.py
@@ -1,20 +1,14 @@
 import pytest
-
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 
-@pytest.fixture()
-def user(db):
-    return User.objects.create_superuser(email='test@test.com', password='test12345')
-
-
 @pytest.mark.django_db
-def test_my_user(user):
-    me = User.objects.get(email='test@test.com')
-    assert me.is_superuser
-    assert me.id == 1
+def test_superuser(user):
+    get_user = User.objects.get(email='test@test.com')
+    assert get_user.is_superuser
+    assert get_user.id == 1
 
 
 @pytest.mark.django_db
@@ -32,7 +26,14 @@ def test_home_page(client):
 
 
 @pytest.mark.django_db
-def test_forbidden_page(client, user):
-    client.login(email='test@test.com', password='test12345')
+def test_course_list_view(client):
+    response = client.get('/courses/')
+    text = 'Все курсы'.encode('utf-8')
+    assert response.status_code == 200
+    assert text in response.content
+
+
+@pytest.mark.django_db
+def test_forbidden_page(client, login_user):
     response = client.get('/courses/my-courses/')
     assert response.status_code == 403
