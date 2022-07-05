32ed1c034c1d9d5847fc1ba1bb581263f650f850
Magomed Kasumov <kasumov82@gmail.com>
2022-07-05 11:53:57 +0300

    Add reset password feature

--- lesson_7/accounts/urls.py
+++ lesson_7/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 
 from . import views
@@ -14,4 +14,31 @@ urlpatterns = [
          name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('signup/', views.SignupPageView.as_view(), name='signup'),
+    path(
+        'reset-password/',
+        auth_views.PasswordResetView.as_view(
+            template_name='reset_password.html',
+            success_url=reverse_lazy('accounts:password_reset_done'),
+            email_template_name='password_reset_email.html',
+        ),
+        name='reset_password',
+    ),
+    path(
+        'reset-password-sent/',
+        auth_views.PasswordResetDoneView.as_view(template_name='password_reset_sent.html'),
+        name='password_reset_done',
+    ),
+    path(
+        'reset/<uidb64>/<token>',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='password_reset_form.html',
+            success_url=reverse_lazy('accounts:password_reset_complete')
+        ),
+        name='password_reset_confirm',
+    ),
+    path(
+        'reset-password-complete/',
+        auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_done.html'),
+        name='password_reset_complete',
+    ),
 ]
