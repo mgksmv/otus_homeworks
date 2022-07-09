7349fbd4db6a1343174084d5414149caea353119
Magomed Kasumov <kasumov82@gmail.com>
2022-07-09 12:54:36 +0300

    Fix test settings

--- lesson_7/config/settings_dev.py
+++ lesson_7/config/settings_dev.py
@@ -1,7 +1,23 @@
 from config.settings import *
 
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+# Local testing
+
+# CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+# CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'testpostgres',
+#         'USER': 'testpostgres',
+#         'PASSWORD': 'testpostgres',
+#         'HOST': 'localhost',
+#         'PORT': 5433,
+#     }
+# }
+
+
+# Docker
 
 DATABASES = {
     'default': {
@@ -9,7 +25,7 @@ DATABASES = {
         'NAME': 'testpostgres',
         'USER': 'testpostgres',
         'PASSWORD': 'testpostgres',
-        'HOST': 'localhost',
-        'PORT': 5433,
+        'HOST': 'testdb',
+        'PORT': 5432,
     }
 }
