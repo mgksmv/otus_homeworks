d0b3dc3c792e647a96ebbd686de0cefb84299681
Magomed Kasumov <kasumov82@gmail.com>
2022-07-11 16:58:53 +0300

    Fix function names

--- lesson_7/onlineschool/views/calendar.py
+++ lesson_7/onlineschool/views/calendar.py
@@ -20,14 +20,14 @@ def get_date(req_day):
     return datetime.today()
 
 
-def prev_month(current_date):
+def get_prev_month(current_date):
     first = current_date.replace(day=1)
     prev_month = first - timedelta(days=1)
     month = 'date=' + str(prev_month.year) + '-' + str(prev_month.month)
     return month
 
 
-def next_month(current_date):
+def get_next_month(current_date):
     days_in_month = calendar.monthrange(current_date.year, current_date.month)[1]
     last = current_date.replace(day=days_in_month)
     next_month = last + timedelta(days=1)
@@ -46,8 +46,8 @@ class ScheduleCalendarView(ListView):
         html_cal = cal.formatmonth(withyear=True)
 
         context['calendar'] = mark_safe(html_cal)
-        context['prev_month'] = prev_month(current_date)
-        context['next_month'] = next_month(current_date)
+        context['prev_month'] = get_prev_month(current_date)
+        context['next_month'] = get_next_month(current_date)
         context['form'] = DateForm()
 
         return context
